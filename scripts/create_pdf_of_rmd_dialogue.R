3a70093aa7d55a65ca25e6ea4a81efba8a9e8e69
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-29 11:44:03 +0100

    Create PDF of dialogue

--- scripts/create_pdf_of_rmd_dialogue.R
+++ scripts/create_pdf_of_rmd_dialogue.R
@@ -6,15 +6,19 @@
 #
 #   ./scripts/create_pdf_of_rmd_dialogue.R
 
-path_from <- "/docs/dialogue/dialogue.Rmd"
+path_from <- "docs/dialogue/dialogue.Rmd"
 
 if (!file.exists(path_from)) {
   setwd("..")
-  getwd()
 }
+testthat::expect_true(file.exists(path_from))
+path_from <- paste0(getwd(), "/docs/dialogue/dialogue.Rmd")
 path_to <- paste0(getwd(), "/docs/dialogue/dialogue.pdf")
+testthat::expect_true(file.exists(path_from))
+
 rmarkdown::render(
-input = path_from, 
-  output_file = path_to,
-  run_pandoc = FALSE
+  path_from, 
+  "pdf_document"
 )
+
+testthat::expect_true(file.exists(path_to))
