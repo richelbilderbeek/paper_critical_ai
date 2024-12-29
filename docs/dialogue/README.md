0e11e0bc7668d8fad5ff916f2f436399a68e4898
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-29 11:43:53 +0100

    Create PDF of dialogue

--- docs/dialogue/README.md
+++ docs/dialogue/README.md
@@ -1,3 +1,6 @@
 # The dialogue
 
-The dialogue can be found at [dialogue.Rmd](dialogue.Rmd).
+The dialogue can be found here:
+
+- [dialogue.Rmd](dialogue.Rmd): the R markdown document
+- [dialogue.pdf](dialogue.pdf): a PDF of the rendered R markdown document
--- scripts/create_pdf_of_rmd_dialogue.R
+++ scripts/create_pdf_of_rmd_dialogue.R
@@ -6,10 +6,15 @@
 #
 #   ./scripts/create_pdf_of_rmd_dialogue.R
 
-path_from <- paste0(getwd(), "/docs/dialogue/dialogue.Rmd")
-testthat::expect_true(file.exists(path_from))
+path_from <- "/docs/dialogue/dialogue.Rmd"
+
+if (!file.exists(path_from)) {
+  setwd("..")
+  getwd()
+}
 path_to <- paste0(getwd(), "/docs/dialogue/dialogue.pdf")
 rmarkdown::render(
 input = path_from, 
-  output_file = path_to
+  output_file = path_to,
+  run_pandoc = FALSE
 )
